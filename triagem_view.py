39fe120e32726e7db2e06ec4b20816044556fdbc
Felipe Gonçales <felipeggoncales22@gmail.com>
2025-09-30 17:24:08 -0300

    triagem

--- triagem_view.py
+++ triagem_view.py
@@ -3,7 +3,7 @@ from flask import request, jsonify
 from main import app, con
 from components.utils import validar_token, remover_bearer
 
-@app.route('/triagem', methods=['POST'])
+@app.route('/start_triagem', methods=['POST'])
 def start_triagem():
     # Obtém o token
     token = request.headers.get('Authorization')
@@ -124,7 +124,7 @@ def start_triagem():
     finally:
         cursor.close()
 
-@app.route('/triagem', methods=['PUT'])
+@app.route('/triagem', methods=['POST'])
 def update_triagem():
     # Obtém o token
     token = request.headers.get('Authorization')
@@ -172,8 +172,8 @@ def update_triagem():
 
         # Se o usuário não for ADM
         if tipo_usuario != 1:
-            # Se o usuário não for paciente, retorna
-            if tipo_usuario != 4:
+            # Se o usuário não for enfermeiro, retorna
+            if tipo_usuario != 3:
                 return jsonify({
                     'error': 'Usuário não autorizado.'
                 }), 401
@@ -181,11 +181,24 @@ def update_triagem():
         # Obtém os dados
         data = request.get_json()
 
-        # Obtém o CPF
+        # Obtém o dados
+        queixa = data.get('queixa')
+        temperatura = data.get('temperatura')
+        pressao = data.get('pressao')
+        frequencia_cardiaca = data.get('frequencia_cardiaca')
+        saturacao = data.get('saturacao')
+        nivel_dor = data.get('nivel_dor')
+        alergia = data.get('alergia')
+        medicamento_uso = data.get('medicamento_uso')
+        classificacao_risco = data.get('classificacao_risco')
+
         cpf = data.get('cpf')
 
-        # Caso não tenha cpf, retorna
-        if not cpf:
+        # Retorna caso algum dado não tenha sido informado
+        if (not queixa or not temperatura or not pressao or not frequencia_cardiaca
+            or not saturacao or not nivel_dor or not alergia or not medicamento_uso
+            or not classificacao_risco or not cpf):
+
             return jsonify({
                 'error': 'Dados incompletos.'
             }), 400
@@ -207,11 +220,11 @@ def update_triagem():
         # Id do paciente
         id_paciente = user_exist[0]
 
-        # Seleciona uma consulta existente
+        # Seleciona uma triagem em andamento existente
         cursor.execute('''
             SELECT ID_CONSULTA
             FROM CONSULTA
-            WHERE ID_USUARIO = ? AND SITUACAO = 1
+            WHERE ID_USUARIO = ? AND SITUACAO = 2
         ''', (id_paciente,))
 
         consulta_exist = cursor.fetchone()
@@ -224,14 +237,47 @@ def update_triagem():
 
         id_consulta = consulta_exist[0]
 
-        data_atual = datetime.now()
+        # Verifica se já possui triagem cadastrada
+        cursor.execute('''
+            SELECT 1
+            FROM TRIAGEM
+            WHERE ID_CONSULTA = ?
+        ''', (id_consulta,))
 
+        # Caso possua, retorna
+        if cursor.fetchone():
+            # Atualiza a situação
+            cursor.execute('''
+                UPDATE CONSULTA
+                SET SITUACAO = 3
+                WHERE ID_CONSULTA = ?
+            ''', (id_consulta,))
+
+            # Salva as alterações
+            con.commit()
+
+            return jsonify({
+                'error': 'Triagem já cadastrada.'
+            }), 401
+
+        # Cria a triagem
+        cursor.execute('''
+            INSERT INTO TRIAGEM
+            (ID_CONSULTA, QUEIXA, TEMPERATURA, PRESSAO, FREQUENCIA_CARDIACA,
+            SATURACAO, NIVEL_DOR, ALERGIA, MEDICAMENTO_USO, CLASSIFICACAO_RISCO,
+            ID_ENFERMEIRO)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (id_consulta, queixa, temperatura, pressao, frequencia_cardiaca, saturacao,
+              nivel_dor, alergia, medicamento_uso, classificacao_risco, id_usuario))
+
+        # Atualiza a situação
         cursor.execute('''
             UPDATE CONSULTA
-            SET SITUACAO = 2, DATA_INICIO_TRIAGEM = ?
+            SET SITUACAO = 3
             WHERE ID_CONSULTA = ?
-        ''', (data_atual, id_consulta))
+        ''', (id_consulta,))
 
+        # Salva as alterações
         con.commit()
 
         return jsonify({

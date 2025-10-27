78134b610ef081340ffe227060d864841f34f709
FelipeGoncales <felipeggoncales22@gmail.com>
2025-10-27 17:02:39 -0300

    load painel + pr_tempo_medio_espera sql

--- load_painel_view.py
+++ load_painel_view.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify
 from main import app, con
-from components.utils import validar_token, remover_bearer
+from components.utils import validar_token, remover_bearer, is_empty
 
 @app.route('/load_painel', methods=['GET'])
 def load_painel():
@@ -55,14 +55,28 @@ def load_painel():
                 'error': 'Requisição não autorizada.'
             }), 401
 
+        data_inicio = request.args.get('i')
+        data_fim = request.args.get('f')
+
+        if is_empty(data_inicio):
+            return jsonify({
+                'error': 'Informe a data de início.'
+            }), 400
+
+        if is_empty(data_fim):
+            return jsonify({
+                'error': 'Informe a data de término.'
+            }), 400
+
         if tipo_usuario == 1:
 
             cursor.execute('''
                 SELECT 
                     COUNT(*),
                     COUNT(DISTINCT ID_USUARIO)
-                FROM CONSULTA c;
-            ''')
+                FROM CONSULTA c
+                WHERE CAST(c.DATA_ENTRADA AS DATE) BETWEEN ? AND ?
+            ''', (data_inicio, data_fim))
 
             result = cursor.fetchone()
 
@@ -73,9 +87,17 @@ def load_painel():
                 total_consultas = result[0]
                 total_pacientes = result[1]
 
+            cursor.execute('''
+                SELECT ESPERA_ATE_ALTA    
+                FROM PR_TEMPO_MEDIO_ESPERA(?, ?)
+            ''', (data_inicio, data_fim))
+
+            tempo_medio = cursor.fetchone()[0]
+
             return jsonify({
                 'total_pacientes': total_pacientes,
-                'total_consultas': total_consultas
+                'total_consultas': total_consultas,
+                'tempo_medio': tempo_medio
             }), 200
 
         else:
@@ -97,9 +119,23 @@ def load_painel():
                 total_pacientes = result[0]
                 casos_urgentes = result[1]
 
+            if tipo_usuario in [3,4]:
+                cursor.execute('''
+                    SELECT ESPERA_TRIAGEM    
+                    FROM PR_TEMPO_MEDIO_ESPERA(?, ?)
+                ''', (data_inicio, data_fim))
+            else:
+                cursor.execute('''
+                    SELECT ESPERA_CONSULTA  
+                    FROM PR_TEMPO_MEDIO_ESPERA(?, ?)
+                ''', (data_inicio, data_fim))
+
+            tempo_medio = cursor.fetchone()[0]
+
             return jsonify({
                 'total_pacientes': total_pacientes,
-                'casos_urgentes': casos_urgentes
+                'casos_urgentes': casos_urgentes,
+                'tempo_medio': tempo_medio
             }), 200
 
     except Exception as e:
